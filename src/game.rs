2d07f9a3ff075259f7d829ab5c7c632164ef9e96
Daniel Deutsch <daniel.deutsch@outreach.io>
2020-01-07 22:59:22 -0800

    Add correct logic for winner

--- src/game.rs
+++ src/game.rs
@@ -1,13 +1,16 @@
 use board::Board;
 use std::io;
 
+const X: char = 'x';
+const O: char = 'o';
+
 pub struct Game {
     pub current_turn: char,
 }
 
 pub fn new_game() -> Game {
     Game {
-        current_turn: 'x',
+        current_turn: X,
     }
 }
 
@@ -19,14 +22,23 @@ pub fn move_request() -> usize {
 
 impl Game { 
     pub fn is_over(&self, board: &Board) -> bool {
-        board.squares[0].value == board.squares[1].value && board.squares[1].value == board.squares[2].value
+        // horizontal win
+        (board.squares[0].value == board.squares[1].value && board.squares[1].value == board.squares[2].value) ||
+        (board.squares[3].value == board.squares[4].value && board.squares[4].value == board.squares[5].value) ||
+        (board.squares[6].value == board.squares[7].value && board.squares[7].value == board.squares[8].value) ||
+        // vertical win
+        (board.squares[0].value == board.squares[3].value && board.squares[3].value == board.squares[6].value) ||
+        (board.squares[1].value == board.squares[4].value && board.squares[4].value == board.squares[7].value) ||
+        (board.squares[2].value == board.squares[5].value && board.squares[5].value == board.squares[8].value) ||
+        // diagonal win
+        (board.squares[0].value == board.squares[4].value && board.squares[4].value == board.squares[8].value) ||
+        (board.squares[2].value == board.squares[4].value && board.squares[4].value == board.squares[6].value)
     }
 
     pub fn winner(&self) -> char {
-        if self.current_turn == 'x' {
-            'o'
-        } else {
-            'x'
+        match self.current_turn {
+            X => O,
+            _ => X,
         }
     }
 
